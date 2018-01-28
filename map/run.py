25d0538bf002f2cb1052989fb3112bae41c639ba
SkyrumbleS <woraphop.kootranunt@gmail.com>
2018-01-28 02:14:41 -0800

    first test with traci

--- map/run.py
+++ map/run.py
@@ -1,44 +1,44 @@
 import os, sys
 
+
+
 if 'SUMO_HOME' in os.environ:
 	tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
 	sys.path.append(tools)
 else:   
 	sys.exit("please declare environment variable 'SUMO_HOME'")
 
-sumo_tools = r"/home/phop/work/sumo-0.31.0/tools"
+sumo_tools = r"/home/phop/work/sumo-0.32.0/tools"
 
 sys.path.append(sumo_tools)
 
 sumoBinary = "/usr/bin/sumo-gui"
-sumoCmd = [sumoBinary, "-c", "/home/phop/work/map/map.sumo.cfg"]
+sumoCmd = [sumoBinary, "-c", "/home/phop/work/Project/map/map.sumo.cfg"]
 
 
 import traci
 
-print '======START====='
+print 'START TRACI...'
 traci.start(sumoCmd)
 
 
 step = 0
-while step < 5000:
-	
+while step < 1000:
 	traci.simulationStep()
-	offset = 200
-#	if(step%(offset*4)==(offset*0)):
-#		print "East Advance Green"
-#		traci.trafficlights.setRedYellowGreenState("267870589", "GGggrrrrGGrrrrrr")
-#	if(step%(offset*4)==(offset*1)):
-#		print "North Advance Green"
-#		traci.trafficlights.setRedYellowGreenState("267870589", "rrrrGGggrrrrGGrr")
-#	if(step%(offset*4)==(offset*2)):
-#		print "West Advance Green"
-#		traci.trafficlights.setRedYellowGreenState("267870589", "GGrrrrrrGGggrrrr")
-#	if(step%(offset*4)==(offset*3)):
-#		print "South Advance Green"
-#		traci.trafficlights.setRedYellowGreenState("267870589", "rrrrGGrrrrrrGGgg")
-
-	step += 0.1
+	offset = 20
+	if(step%(offset*4)==(offset*0)):
+		print "STATE 1"
+		traci.trafficlights.setRedYellowGreenState("gneJ1", "GGGGrrrrGGGGrrrr")
+	if(step%(offset*4)==(offset*1)):
+		print "STATE 2"
+		traci.trafficlights.setRedYellowGreenState("gneJ1", "rrrrGGGGrrrrGGGG")
+	if(step%(offset*4)==(offset*2)):
+		print "STATE 3"
+		traci.trafficlights.setRedYellowGreenState("gneJ1", "GGGGrrrrGGGGrrrr")
+	if(step%(offset*4)==(offset*3)):
+		print "STATE 4"
+		traci.trafficlights.setRedYellowGreenState("gneJ1", "rrrrGGGGrrrrGGGG")
+	step += 1
 
 traci.close()
-print '======FINISH====='
+print 'FINISH'
